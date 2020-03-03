abb8193406feb220022350d4ac5862e94e35ef22
Max S <creatingalong@gmail.com>
2020-03-03 16:35:20 -0600

    added includes directory and assets

---
+++ includes/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <link rel="stylesheet" type="text/css" href="assets/styles.css">
+        <title>PHP 4chan Downloader</title>
+    </head>
+    <body>
