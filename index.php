a680dd19d20bea864092540ff7ed936fc63400e6
Max S <creatingalong@gmail.com>
2020-03-03 16:35:46 -0600

    switched header and footer to php includes, added a form and div layout

--- index.php
+++ index.php
@@ -8,19 +8,46 @@
     $posts = $document->find('a[href*=i.4cdn.org]');
 
     foreach($posts as $post) {
-        echo "http://i.4cdn.org/g/" . $post->text(), "\n";
+        //echo "http://i.4cdn.org/g/" . $post->text(), "\n";
     }
-    echo "completed";
+    //echo "completed";
 
- ?>
+?>
 
- <!DOCTYPE html>
- <html lang="en" dir="ltr">
-     <head>
-         <meta charset="utf-8">
-         <title></title>
-     </head>
-     <body>
 
-     </body>
- </html>
+    <?php include("includes/header.php"); ?>
+
+         <div class="main-container">
+
+             <div class="text-main">
+                 <h1>4chan Image Downloader</h1>
+             </div>
+
+
+             <form class="input-form" action="" method="post">
+
+                 <select id="chanSelector" name="chans">
+                    <option value="4chan">4chan</option>
+                    <option value="4channel">4channel</option>
+                 </select>
+
+                 <select id="boardSelector" name="boards">
+                    <option value="g">g</option>
+                    <option value="b">b</option>
+                    <option value="k">k</option>
+                    <option value="wsg">wsg</option>
+                    <option value="gif">gif</option>
+                    <option value="wg">wg</option>
+                    <option value="a">a</option>
+                    <option value="lit">lit</option>
+                 </select>
+
+                 <input type="text" name="thread-url" value="">
+                 <input type="submit" name="submit" value="Submit">
+             </form>
+
+
+
+         </div>
+
+    <?php include("includes/footer.php"); ?>
