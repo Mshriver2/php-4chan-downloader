82e64cd8d7f1b7ff6f21c70f1ff6af9dcfe6fa65
Max S <creatingalong@gmail.com>
2020-03-03 20:28:31 -0600

    added fonts to header

--- includes/header.php
+++ includes/header.php
@@ -5,6 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" type="text/css" href="assets/styles.css">
+        <link href="https://fonts.googleapis.com/css?family=Oswald:600|Roboto+Condensed&display=swap" rel="stylesheet"> 
         <title>PHP 4chan Downloader</title>
     </head>
     <body>
